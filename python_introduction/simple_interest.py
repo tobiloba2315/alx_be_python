e008e1cbc7fd42c7d681d0960ebccec2db69b62c
olagbegi blessing <olagbegitobiloba@gmail.com>
2025-11-11 13:05:42 +0100

    a simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 # define the variables
-principal_amount = 1000
-rate_of_interest = 0.05
+principal_amount  = 1000
+rate_of_interest = 5 / 100
 time_period = 3
 # calculate simple interest
 simple_interest = principal_amount * rate_of_interest * time_period
