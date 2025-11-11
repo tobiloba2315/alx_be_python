d9041d95d1b2e55861792d21e3189e9796883c8c
olagbegi blessing <olagbegitobiloba@gmail.com>
2025-11-11 11:53:12 +0100

    basic operations on python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#define the two variables
+number1 = 10
+number2 = 5
+#perform basic arithemetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+division = number1 / number2
+multiplication = number1 * number2
+#display the results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Division:", division) 
+print("Multiplication:", multiplication)
