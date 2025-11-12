5296bca485604f8dd25aba33e6dcc4df0e8feada
olagbegi blessing <olagbegitobiloba@gmail.com>
2025-11-13 00:08:14 +0100

    match case

---
+++ control_flow/match_case_calculator.py
@@ -0,0 +1,31 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+# match operation:
+#     case "+":
+#         result = num1 + num2
+#     case "-":
+#         result = num1 - num2
+#     case "*":
+#         result = num1 * num2
+#     case "/":
+#         if num2 != 0:
+#             result = num1 / num2
+#         else:
+#             result = "Error: Division by zero"
+#     case _:
+#         result = "Error: Invalid operation"
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero"
+else:
+    result = "Error: Invalid operation"
+print(f"The result is: {result}")
