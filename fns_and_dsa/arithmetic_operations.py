e61a0f30c118e276c7bd25ad51f79b4eb134179a
olagbegi blessing <olagbegitobiloba@gmail.com>
2025-11-22 15:27:47 +0100

    operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -21,7 +21,7 @@ float or str: Result of the operation or an error message
  elif operation == 'multiply':
         return num1 * num2
  elif operation == 'divide':
-        if num2 != 0:
+        if num2 == 0:
             return num1 / num2
         else:
             return "Error: Division by zero"
