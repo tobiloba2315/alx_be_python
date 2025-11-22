a790a0013dbfc3ff565f53804b5141753dd0c9ae
olagbegi blessing <olagbegitobiloba@gmail.com>
2025-11-22 13:41:20 +0100

    pattern

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,17 @@
+def get_positive_int(prompt="Enter the size of the pattern: "):
+    while True:
+        try:
+            n = int(input(prompt))
+            if n > 0:
+                return n
+            print("Please enter a positive integer.")
+        except ValueError:
+            print("Please enter a positive integer.")
+
+def draw_square(n):
+    for _ in range(n):
+        print("* " * n)
+
+if __name__ == "__main__":
+    size = get_positive_int()
+    draw_square(size)
