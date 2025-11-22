3985cd02f2ec8dbc17a47d6c33eb8b188680ba45
olagbegi blessing <olagbegitobiloba@gmail.com>
2025-11-22 15:50:06 +0100

    shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,29 @@
+shopping_list = []
+while True:
+ print("\n=== Shopping List Manager ===")
+ print("1. Add an item")
+ print("2. Remove an item")
+ print("3. View shopping list")
+ print("4. Exit")
+choice = input("Enter your choice (1-4):")
+if choice == "1":
+ item = input("Enter the item to add:")
+ shopping_list.append(item)
+ print(f"'{item}' has been added to your shopping list.")
+elif choice == "2":
+ item = input("Enter the item to remove:")
+ if item in shopping_list:
+  shopping_list.remove(item)
+  print(f"'{item}' has been removed from your shopping list.")
+ else:
+  print(f"'{item}' was not found in the shopping list.")
+elif choice == "3":
+   print("\nYour Shopping List:")
+   if not shopping_list:
+    print("(The list is empty.)")
+   else:
+    for i, item in enumerate(shopping_list, 1):print(f"{i}.{item}")
+elif choice == "4":
+    print("Exiting the Shopping List Manager. Goodbye!")
+else:
+ print("Invalid choice. Please enter a number between 1 and 4.")
